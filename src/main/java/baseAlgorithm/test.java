635f37334fa099669c25b232f05c40eabadc9b6c
cherie <wangjingna6688@163.com>
2019-01-29 17:57:20 +0800

    测试

---
+++ src/main/java/baseAlgorithm/test.java
@@ -0,0 +1,10 @@
+package baseAlgorithm;
+
+/**
+ * create by cherie on 29/01/2019.
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("haha");
+    }
+}
